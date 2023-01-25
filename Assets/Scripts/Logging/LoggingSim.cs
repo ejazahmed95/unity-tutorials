4cb68e1cc283b0ea0fef2e6c3b64680affcd4bcc
Ejaz Ahmed <ejaz.mohammed@utah.edu>
2023-01-25 00:24:30 -0700

    prettifying logs with colors and tags

--- Assets/Scripts/Logging/LoggingSim.cs
+++ Assets/Scripts/Logging/LoggingSim.cs
@@ -3,26 +3,28 @@ using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class LoggingSim : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        Random.InitState((int)DateTime.Now.Ticks);
-        StartCoroutine(StartLogging("MyApp"));
-        StartCoroutine(StartLogging("Player Controller"));
-        StartCoroutine(StartLogging("Gameplay Manager"));
-        StartCoroutine(StartLogging("Event Manager"));
-    }
+namespace Logging {
+    public class LoggingSim : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+            Random.InitState((int)DateTime.Now.Ticks);
+            StartCoroutine(StartLogging("MyApp"));
+            StartCoroutine(StartLogging("Player Controller"));
+            StartCoroutine(StartLogging("Gameplay Manager"));
+            StartCoroutine(StartLogging("Event Manager"));
+        }
 
-    private IEnumerator StartLogging(string systemName) {
-        while (true) {
-            float randomValue = Random.value;
-            yield return new WaitForSeconds(0.5f + randomValue);
-            if (randomValue < 0.3f) {
-                Debug.Log($"{systemName}: This is a normal log");
-            } else if (randomValue < 0.4f) {
-                Debug.LogWarning($"{systemName}: This is a warning log");
-            } else if (randomValue < 0.5f) {
-                Debug.LogError($"{systemName}: This is an error log");
+        private IEnumerator StartLogging(string systemName) {
+            while (true) {
+                float randomValue = Random.value;
+                yield return new WaitForSeconds(0.5f + randomValue);
+                if (randomValue < 0.3f) {
+                    Debug.Log($"{systemName.Bold()}: This is a normal log");
+                } else if (randomValue < 0.4f) {
+                    Debug.LogWarning($"{systemName.Bold()}: This is a warning log".Color(Color.yellow));
+                } else if (randomValue < 0.5f) {
+                    Debug.LogError($"{systemName.Bold()}: This is an error log".Color(Color.red));
+                }
             }
         }
     }
---
+++ Assets/Scripts/Logging/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Logging {
+	public static class StringExtensions {
+		public static string Bold(this string str) => "<b>" + str + "</b>";
+		public static string Color(this string str,string clr) => $"<color={clr}>{str}</color>";
+		public static string Color(this string str, Color clr) => $"<color=#{ColorUtility.ToHtmlStringRGB(clr)}>{str}</color>";
+		public static string Italic(this string str) => "<i>" + str + "</i>";
+		public static string Size(this string str, int size) => $"<size={size}>{str}</size>";
+	}
+}
